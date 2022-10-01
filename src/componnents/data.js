c7dbc9ac391dea6f680b16416ef140c8fb48ec1a
AMNA-HEDHILI <amna.hdhili@gmail.com>
2022-10-01 13:05:43 +0200

    first commit

---
+++ src/componnents/data.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+class data extends Component{
+    render(){
+        return(
+            <div>
+                <h2>about</h2>
+                <p>
+                this is about section
+                </p>
+            </div>
+        )
+    }
+}
+export default data
